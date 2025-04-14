8179b4a82d4f8ab8aad4355781e45090c589220a
tadahayamiz <tadahaya@gmail.com>
2025-04-14 22:49:30 +0900

    debug dh

--- histvae/src/data_handler.py
+++ histvae/src/data_handler.py
@@ -176,8 +176,7 @@ class PointHistDataset(Dataset):
         hist0 = torch.tensor(hist0, dtype=torch.float32).unsqueeze(0) # add channel dimension
         hist1 = torch.tensor(hist1, dtype=torch.float32).unsqueeze(0) # add channel dimension
         # transform
-        hist0 = self.transform(hist0)
-        hist1 = self.transform(hist1)
+        hist0, hist1 = self.transform(hist0, hist1)
         # prepare label
         if self.label is not None:
             label = self.label[selected_indices][0]
